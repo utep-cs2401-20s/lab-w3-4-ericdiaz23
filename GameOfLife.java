f70156fd3b16ecbb55b24aef83359f8ebbb97ad3
ediaz23 <Soxners$23>
2020-02-03 14:17:03 -0700

    New Commit

---
+++ GameOfLife.java
@@ -0,0 +1,7 @@
+/* Eric Diaz, 80647920
+/ Martine Ceberio Lab #3 */
+
+public class GameOfLife{
+        // for each cell, count live neighbors deternube live/dead for new board start
+        // Early patterns with unknown futures, such as the R-pentomino, led computer programmers across the world to write programs to track the evolution of Life patterns. Most of the early algorithms were similar: they represented Life patterns as two-dimensional arrays in computer memory. Typically two arrays are used: one to hold the current generation, and one to calculate its successor. Often 0 and 1 represent dead and live cells respectively. A nested for loop considers each element of the current array in turn, counting the live neighbours of each cell to decide whether the corresponding element of the successor array should be 0 or 1. The successor array is displayed. For the next iteration, the arrays swap roles so that the successor array in the last iteration becomes the current array in the next iteration
+}
