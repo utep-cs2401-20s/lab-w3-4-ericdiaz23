7f0924ae84c828b4643191cbd6a63285c4f34726
ediaz23 <Soxners$23>
2020-02-13 21:25:55 -0700

    New Commit

--- GOLTester.java
+++ GOLTester.java
@@ -1,6 +1,96 @@
 /* Eric Diaz, 80647920
 / Martine Ceberio Lab #3 */
+import org.junit.jupiter.api.Test;
 
-public class GameOfLife{
+public class GOLTester{
 
+    // Tests for Neighbors
+
+    @Test
+    public void testNeighbors1() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+    @Test
+    public void testNeighbors2() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+    @Test
+    public void testNeighbors3() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+    @Test
+    public void testNeighbors4() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+    @Test
+    public void testNeighbors5() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    // Tests for One Step
+
+    @Test
+    public void testOneStep1() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+    @Test
+    public void testOneStep2() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+    @Test
+    public void testOneStep3() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    @Test
+    public void testOneStep4() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    @Test
+    public void testOneStep5() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    // Tests for Evolution
+
+    @Test
+    public void testEvolution1() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    @Test
+    public void testEvolution2() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    @Test
+    public void testEvolution3() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    @Test
+    public void testEvolution4() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
+
+    @Test
+    public void testEvolution5() {
+        board sol = new board(3);
+        assertEquals(-2, sol.neighbors(1,2));
+    }
 }
